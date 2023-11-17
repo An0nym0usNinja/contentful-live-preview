d547e92427b2d8ad225e64c62f3363a6ffd2ecd8
Adriaan Mouton <arouxmouton@gmail.com>
2023-11-17 10:51:14 +0200

    setup laravel

---
+++ resources/views/preview/page.blade.php
@@ -0,0 +1,13 @@
+<x-guest-layout>
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        {{ __('Preview page header') }}
+    </h2>
+
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        {{ __('Preview page content') }}
+    </h2>
+
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        {{ __('Preview page header') }}
+    </h2>
+</x-guest-layout>
